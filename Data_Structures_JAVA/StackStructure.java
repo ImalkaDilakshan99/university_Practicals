9f9457cb3156effeaa9bedd9c62fc420cf8a6125
ImalkaDilakshan99 <imalkadilakshan99@gmail.com>
2024-06-14 00:16:53 +0530

    stack data structure

--- Data_Structures_JAVA/StackStructure.java
+++ Data_Structures_JAVA/StackStructure.java
@@ -50,9 +50,74 @@ public class StackStructure {
         return value;
     }
 
+    //peek() method
+    //return type
+    //no parameter
+    int peek(){
+        int value=0;
+        if(top==-1){
+            System.out.println("Stack is empty.");
+        }else{
+            value = arr[top];
+        }
+
+        return value;
+    }
+
+    // get size() method
+    // int return type
+    // no parameters
+    int getSize(){
+        return top+1;
+    }
+
+    // is Empty() method
+    // boolean type
+    // no parameter
+    Boolean isEmpty(){
+        return top == -1;
+    }
+
+    // is Full() method
+    // boolean type
+    // no parameter
+    Boolean isFull(){
+        return top==uB;
+    }
+
+    // printStack() method
+    // no return type
+    // no parameter
+    void printStack(){
+        for(int i=0; i<=top; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+
     public static void main(String[] args){
 
         //Create a Stack
+        StackStructure stack1 = new StackStructure(5);
+        //insert items
+        stack1.push(10);
+        stack1.push(20);
+        stack1.push(30);
+        stack1.push(40);
+        stack1.push(50);
+        stack1.push(60);
+
+        //print the stack1
+        stack1.printStack();
+
+        //remove itemss
+        stack1.pop();
+
+        
+        //print the stack1
+        stack1.printStack();
+
+        //get the size of the stack
+        System.out.println("Size = "+stack1.getSize());
 
     }
 
