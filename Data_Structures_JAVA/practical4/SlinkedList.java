bb6a57956624816358b7333a9645725bff9e8d5a
ImalkaDilakshan99 <imalkadilakshan99@gmail.com>
2024-06-18 13:36:17 +0530

    add delete operation

--- Data_Structures_JAVA/practical4/SlinkedList.java
+++ Data_Structures_JAVA/practical4/SlinkedList.java
@@ -1,10 +1,10 @@
 package practical4;
 
-public class SlinkedList {
+public class SLinkedList {
     Node head;
 
     //Constructor
-    public SlinkedList(){
+    public SLinkedList(){
         this.head = null;
     }
 
@@ -31,7 +31,7 @@ public class SlinkedList {
     //desplay() method to print all data in the LinkedList
     public void display(){
         Node current = head;
-        System.out.println("Node (first-->last)");
+        System.out.println("Node (first-->last): ");
         while(current != null){
             current.displayNode();
             current = current.next;
@@ -40,13 +40,74 @@ public class SlinkedList {
     }
 
     //InsertEnd() method
+    public void insertEnd(int new_data){
+
+        //check if linkedList is empty or not
+        if(isEmpty()){
+            head = new Node(new_data);
+            return;
+        }
+        Node new_node = new Node(new_data);
+        new_node.next = null;
+        Node last = head;
+        while(last.next != null){
+            last = last.next;
+        }
+        last.next = new_node;
+
+
+    }
 
     //searchNode method to search a Node
+    public Node searchNode(int data) {
+
+        //check if linked list empty or not
+        if(isEmpty()){
+            head = new Node(data);
+            return null;
+        }
+
+        Node prev_node = null;
+        Node current = head;
+        while(current != null){
+            if(current.data == data){
+                prev_node = current;
+                System.out.println("Found the Node");
+                break;
+            }
+            current = current.next;
+        }
+        return prev_node;
+    }
 
     //InsertAfter()method
+    public void insertAfter(Node prev_node, int new_data){
+        // assumed list is not empty
+        if(prev_node == null){
+            System.out.println("The given node is null.");
+            return;
+        }
+    
+
+    Node new_node = new Node(new_data);
+    new_node.next = prev_node.next;
+    prev_node.next = new_node;
+    }
 
     //Deletion operation
+
     //deleteFirst()
+    public Node deleteFirst(){
+        Node currentHNode = head;
+        if(isEmpty()){
+            System.out.println("Linked list is Empty");
+        }
+        else{
+            head = head.next;
+        }
+        return currentHNode;
+    }
+
     //deleteEnd
     //deleteAfter
 
--- Data_Structures_JAVA/practical4/TestLinkedList.java
+++ Data_Structures_JAVA/practical4/TestLinkedList.java
@@ -1,15 +1,31 @@
 package practical4;
 
 public class TestLinkedList {
-    public static void main(String[]args){
-        SlinkedList list = new SlinkedList();
+    public static void main(String[] args){
+        SLinkedList list = new SLinkedList();
 
-        list.insertFirst(10);
-        list.insertFirst(20);
-        list.insertFirst(30);
-        list.insertFirst(40);
         list.insertFirst(50);
+        list.insertFirst(40);
+        list.insertEnd(10);
         list.display();
+
+        //search 50 is available
+        Node newnNode = list.searchNode(50);
+
+        newNode.displayNode();
+
+        //insert 200 after 10
+        Node data10_node = list.searchNode(10);
+        list.insertAfter(data10_node,200);
+        list.display();
+
+        //delete from first
+        Node deletedNode = list.deleteFirst();
+        deletedNode.displayNode();
+        System.out.println("is Deleted");
+        list.display();
+       
+
     }
    
 }
